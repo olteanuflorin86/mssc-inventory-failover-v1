c8f09224d4f1af89587a571ed6d0e17cb107de14
Florin Olteanu <olteanuflorin86@gmail.com>
2023-07-31 17:13:38 +0300

    initial project add

---
+++ src/main/java/com/olteanuflorin86/msscinventoryfailoverv1/MsscInventoryFailoverV1Application.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.msscinventoryfailoverv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscInventoryFailoverV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscInventoryFailoverV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/olteanuflorin86/msscinventoryfailoverv1/MsscInventoryFailoverV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.msscinventoryfailoverv1;
+
+import org.junit.jupiter.api.Test; 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscInventoryFailoverV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
